a0d02e1a24d8ab3dfce5eec49dd08384dc81dcdd
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-03-23 18:44:53 -0600

    conexión a base de datos

---
+++ includes/database.php
@@ -0,0 +1,22 @@
+<?php
+
+function conectarBD() {
+    $host = "localhost"; 
+    $dbname = "dbphp";
+    $user = "postgres";
+    $password = "16022004";
+
+    try {
+        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        echo "Se conectó de manera exitosa";
+    } catch (PDOException $e) {
+        echo "No se pudo conectar a la base de datos: " . $e->getMessage();
+    }
+
+    return $db;
+}
+
+
+$db = conectarBD();
+
+
